100fe65577b0ada3b8dad3b4d448c4ac33740b1d
Irakliy Stefan <arxitektonas@mail.ru>
2023-01-08 19:03:04 +0600

    init: start code js for task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
   
+  let a = n;
+
+  a = a.toString();
+  a = a.split('');
+  a = a.reverse();
+  a = a.join('');
+  a = parseFloat(a);
+
+  n = a;
+
+  return n;
 }
